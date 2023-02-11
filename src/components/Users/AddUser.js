ab85fdb31e450c3753700b25679d42c6f1309b76
İHLASMERT <ihlas1mert@gmail.com>
2023-02-11 18:15:15 +0300

    added map

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,54 @@
-import React from 'react'
-import Card from "../UI/Card"
-import classes from "../Users/AddUser.module.css"
+import React, { useState } from 'react'
+import Button from '../UI/Button'
+import Card from '../UI/Card'
+import classes from '../Users/AddUser.module.css'
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('')
+  const [enteredAge, setEnteredAge] = useState('')
+
   const addUserHandler = (event) => {
     event.preventDefault()
+    if (
+      enteredUserName.trim().length === 0 ||
+      enteredAge.trim().length === 0
+    ) {
+      return
+    }
+    if (+enteredAge <= 0) {
+      return
+    }
+
+    console.log(enteredAge, enteredUserName)
+    setEnteredAge('')
+    setEnteredUserName('')
+  }
+  const userNameChangehandler = (event) => {
+    setEnteredUserName(event.target.value)
+  }
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value)
   }
 
   return (
-   <Card className={classes.input}>
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input type='text' id='username' />
-      <label htmlFor='username'>Age(years)</label>
-      <input type='number' id='age' />
-      <button type='submit'>Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input
+          type='text'
+          id='username'
+          onChange={userNameChangehandler}
+          value={enteredUserName}
+        />
+        <label htmlFor='username'>Age(years)</label>
+        <input
+          type='number'
+          id='age'
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
+        <Button type='submit'>Add User</Button>
+      </form>
     </Card>
   )
 }
